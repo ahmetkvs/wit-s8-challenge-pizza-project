c87e1e828304175b6b8051589acb7a0d96194478
ahmetkvs <ahmetpkavas@gmail.com>
2025-02-25 01:56:39 +0300

    Pizza Order Form yarilandi

--- src/Order.jsx
+++ src/Order.jsx
@@ -24,20 +24,83 @@ export default function Order() {
       <main className="order-content">
         <section className="pizza-section">
           <PizzaFooter></PizzaFooter>
-          <p className="pizza-description"></p>
+          <p className="pizza-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+            pulvinar posuere ipsum id tincidunt. Suspendisse dignissim posuere
+            metus, ac pulvinar lectus dictum id. Praesent semper finibus tellus,
+            eu pellentesque lectus mollis quis. Pellentesque molestie, nibh
+            faucibus ornare gravida, sapien nunc vehicula velit, id elementum
+            mauris ipsum a dolor. Integer risus nunc, vestibulum eget venenatis
+            vitae, consequat et eros. Maecenas vulputate velit dolor, eu
+            scelerisque nisi sagittis et.{" "}
+          </p>
         </section>
-        <section className="form-section">
-          <div className="pizzaForm-boyut-homur-container">
-            <div className="pizzaForm-boyut"></div>
-            <div className="pizzaForm-hamur"></div>
+        <form>
+          <div className="pizzaForm-init-container">
+            <div className="pizzaForm-boyut">
+              <p>Boyut Seç:</p>
+              <span>
+                <input type="radio" name="pizza-boyut-s" id="pizza-boyut-s" />
+                <label htmlFor="pizza-boyut-s">Küçük</label>
+              </span>
+              <span>
+                <input type="radio" name="pizza-boyut-m" id="pizza-boyut-m" />
+                <label htmlFor="pizza-boyut-m">Orta</label>
+              </span>
+              <span>
+                <input type="radio" name="pizza-boyut-l" id="pizza-boyut-l" />
+                <label htmlFor="pizza-boyut-l">Büyük</label>
+              </span>
+            </div>
+            <div className="pizzaForm-hamur">
+              <label htmlFor="pizza-hamur">Hamur Seç:</label>
+
+              <select name="pizza-hamur" id="pizza-hamur">
+                <option value="ince">İnce Hamur</option>
+                <option value="standart">Standart Hamur</option>
+                <option value="kalin">Kalin Hamur</option>
+              </select>
+            </div>
           </div>
-          <div className="pizzaForm-extras"></div>
+          <div className="pizzaForm-name">
+            <label htmlFor="costumer-name">İsim - Soyad:</label>
+            <input type="text" id="costumer-name" />
+          </div>
+          <div className="pizzaForm-extras">
+            <p id="checkbox-array-label">Ek Malzemeler</p>
+            <p>En fazla 10 malzeme seçebilirsiniz. 5₺</p>
+
+            <div className="checkbox-array">
+              {[
+                "Sucuk",
+                "Mantar",
+                "Zeytin",
+                "Biber",
+                "Domates",
+                "Soğan",
+                "Jalapeño",
+                "Mozzarella",
+                "Ton Balığı",
+                "Mısır",
+                "Sosis",
+                "Ananas",
+                "Pastırma",
+                "Kekik",
+                "Peynir",
+              ].map((topping, index) => (
+                <label key={index} className="checkbox-array-item">
+                  <input type="checkbox" name="toppings" value={topping} />
+                  {topping}
+                </label>
+              ))}
+            </div>
+          </div>
           <div className="pizzaForm-not"></div>
           <div className="pizzaForm-checkout-container">
             <div className="pizzaForm-pizzaCounter"></div>
             <div className="pizzaForm-checkout"></div>
           </div>
-        </section>
+        </form>
       </main>
     </>
   );
